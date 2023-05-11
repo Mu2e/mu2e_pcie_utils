9bf6cf89cc755ffb7966ef2c1d27ab81edf7cec2
Eric Flumerfelt <eflumerf@fnal.gov>
2023-05-11 11:05:32 -0400

    Update make_test_file

--- tools/make_test_file.sh
+++ tools/make_test_file.sh
@@ -2,11 +2,11 @@
 
 # DTC Config
 export DTCLIB_SIM_ENABLE=E
-export DTCLIB_NUM_TRACKER_BLOCKS=212
-export DTCLIB_NUM_CALORIMETER_BLOCKS=60
+export DTCLIB_NUM_TRACKER_BLOCKS=270
+export DTCLIB_NUM_CALORIMETER_BLOCKS=134
 export DTCLIB_NUM_CALORIMETER_HITS=10
 export DTCLIB_NUM_CRV_BLOCKS=0
 
 # Create file
-mu2eUtil -Q -n 1000 --binary-file-mode DTC_packets.bin verify_stream
+mu2eUtil -Q -n 10000 --binary-file-mode DTC_packets.bin buffer_test
 
